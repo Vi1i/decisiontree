85e9841100fe8e30615e08dde7406948f6c9bd4e
Vi1i <vi1i.github@gmail.com>
2017-03-09 13:57:42 -0500

    Updated to pass in map to get arguments

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 #include "Func/func.hpp"
 
 int main(int argc, char * argv[]) {
-	if(!func::Args(argc, argv)) {
+	std::map<std::string, std::vector<std::string>> args;
+	if(!func::Args(argc, argv, args)) {
 		exit(EXIT_FAILURE);
 	}
 	exit(EXIT_SUCCESS);
