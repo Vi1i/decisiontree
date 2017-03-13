9223e5a0ed86d8ea9aab2f27a4c3e1b9b704a62f
Vi1i <vi1i.github@gmail.com>
2017-03-13 14:42:50 -0400

    Updated the main function to call the parse functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include <fstream>
 #include <string>
 
-#include "dt_config.hpp"
 #include "Func/func.hpp"
+#include "Parse/parse.hpp"
+#include "DT/dt.hpp"
 
 int main(int argc, char * argv[]) {
 	std::map<std::string, std::vector<std::string>> args;
 	if(!func::Args(argc, argv, args)) {
 		exit(EXIT_FAILURE);
 	}
+	std::map<std::string, std::vector<std::string>> header;
+	parse::Header(args["--training"].front(), header);
+	
+	std::vector<std::vector<std::string>> data;
+	parse::Data(args["--training"].front(), data);
+
 	exit(EXIT_SUCCESS);
 }
